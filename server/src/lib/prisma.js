67c9b0f877abc16475cae89f737b2a2c82f15586
aimansahar330-oss <aimansahar330@gmail.com>
2026-09-21 18:50:11 +0500

    fix prisma client for vercel

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -1,23 +1,35 @@
 import "dotenv/config";
 
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient} from "../../generated/prisma/client.ts";
 
-if(!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL is missing from .env");
+import {
+  PrismaClient,
+} from "../generated/prisma/client.ts";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL is missing from environment variables"
+  );
 }
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-});
+const adapter =
+  new PrismaPg({
+    connectionString:
+      process.env.DATABASE_URL,
+  });
 
-const prisma = globalThis.honeyGlowPrisma || new PrismaClient({
+const prisma =
+  globalThis.honeyGlowPrisma ||
+  new PrismaClient({
     adapter,
-});
+  });
 
-if(process.env.NODE_ENV !== "production") {
-    globalThis.honeyGlowPrisma = prisma;
+if (
+  process.env.NODE_ENV !==
+  "production"
+) {
+  globalThis.honeyGlowPrisma =
+    prisma;
 }
 
 export default prisma;
